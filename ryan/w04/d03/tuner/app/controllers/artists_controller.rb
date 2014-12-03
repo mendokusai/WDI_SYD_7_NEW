f4ca0a092a1e2d08732f3c7b545180b3e2fc848e
Ryan <ryanpauley@gmail.com>
2014-12-03 15:27:41 +1100

    Artist CRUD working

--- ryan/w04/d03/tuner/app/controllers/artists_controller.rb
+++ ryan/w04/d03/tuner/app/controllers/artists_controller.rb
@@ -7,5 +7,44 @@ class ArtistsController < ApplicationController
   	@artist = Artist.find(params[:id])
   end
 
-  
+  def new
+  	@artist = Artist.new
+  end
+
+  def create
+  	@artist = Artist.new(artist_params)
+  	if @artist.save
+  		redirect_to artist_path(@artist)
+  	else
+  		render :new
+  	end
+  end
+
+  def edit
+  	@artist = Artist.find(params[:id])
+  end
+
+  def update
+  	@artist = Artist.find(params[:id])
+  	if @artist.update(artist_params)
+  	else
+  		render :edit
+  	end
+  	redirect_to @artist
+  end
+
+  def destroy
+  	@artist = Artist.find(params[:id])
+  	@artist.destroy
+  	redirect_to artists_path
+  end
+
+  private
+  	def artist_params
+  		params.require(:artist).permit(:name,
+  			:hometown, :bio, :years_active)
+  	end
+
+
+
 end
